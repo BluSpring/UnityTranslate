4831d2006cf7f79b9b5040b08a2972f7d075a62a
Naz <bluspringindohq@gmail.com>
2024-08-22 12:09:56 +0800

    Catch any crashes with save/load

--- src/main/kotlin/xyz/bluspring/unitytranslate/UnityTranslate.kt
+++ src/main/kotlin/xyz/bluspring/unitytranslate/UnityTranslate.kt
@@ -118,18 +118,28 @@ class UnityTranslate : ModInitializer {
         }
 
         fun saveConfig() {
-            if (!configFile.exists())
-                configFile.createNewFile()
+            try {
+                if (!configFile.exists())
+                    configFile.createNewFile()
 
-            val serialized = Json.encodeToString(UnityTranslateConfig.serializer(), config)
-            configFile.writeText(serialized)
+                val serialized = Json.encodeToString(UnityTranslateConfig.serializer(), config)
+                configFile.writeText(serialized)
+            } catch (e: Exception) {
+                logger.error("Failed to save UnityTranslate config!")
+                e.printStackTrace()
+            }
         }
 
         fun loadConfig() {
             if (!configFile.exists())
                 return
 
-            config = Json.decodeFromString(UnityTranslateConfig.serializer(), configFile.readText())
+            try {
+                config = Json.decodeFromString(UnityTranslateConfig.serializer(), configFile.readText())
+            } catch (e: Exception) {
+                logger.error("Failed to load UnityTranslate config, reverting to defaults.")
+                e.printStackTrace()
+            }
         }
     }
 }
