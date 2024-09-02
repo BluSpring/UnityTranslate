7760dc7719170dbe317888a56d59e840d59fb383
Naz <bluspringindohq@gmail.com>
2024-09-03 04:19:48 +0800

    Implement Plasmo Voice support

--- common/src/main/kotlin/xyz/bluspring/unitytranslate/compat/voicechat/UTVoiceChatCompat.kt
+++ common/src/main/kotlin/xyz/bluspring/unitytranslate/compat/voicechat/UTVoiceChatCompat.kt
@@ -1,70 +1,49 @@
 package xyz.bluspring.unitytranslate.compat.voicechat
 
-import de.maxhenkel.voicechat.api.ForgeVoicechatPlugin
-import de.maxhenkel.voicechat.api.VoicechatPlugin
-import de.maxhenkel.voicechat.api.VoicechatServerApi
-import de.maxhenkel.voicechat.api.events.EventRegistration
-import de.maxhenkel.voicechat.api.events.MicrophoneMuteEvent
-import de.maxhenkel.voicechat.api.events.VoicechatServerStartedEvent
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.level.GameType
 import xyz.bluspring.unitytranslate.UnityTranslate
-import xyz.bluspring.unitytranslate.client.UnityTranslateClient
 
-@ForgeVoicechatPlugin
-class UTVoiceChatCompat : VoicechatPlugin {
-    override fun getPluginId(): String {
-        return UnityTranslate.MOD_ID
+object UTVoiceChatCompat {
+    val usesSimpleVoiceChat: Boolean
+        get() {
+            return UnityTranslate.instance.proxy.isModLoaded("voicechat")
+        }
+
+    val usesPlasmoVoice: Boolean
+        get() {
+            return UnityTranslate.instance.proxy.isModLoaded("plasmovoice")
+        }
+
+    val hasVoiceChat: Boolean
+        get() {
+            return usesSimpleVoiceChat || usesPlasmoVoice
+        }
+
+    fun getNearbyPlayers(source: ServerPlayer): List<ServerPlayer> {
+        return if (usesSimpleVoiceChat)
+            SimpleVoiceChatCompat.getNearbyPlayers(source)
+        else if (usesPlasmoVoice)
+            PlasmoVoiceChatCompat.getNearbyPlayers(source)
+        else
+            listOf(source)
     }
 
-    override fun registerEvents(registration: EventRegistration) {
-        super.registerEvents(registration)
-
-        registration.registerEvent(MicrophoneMuteEvent::class.java) {
-            if (UnityTranslate.config.client.muteTranscriptWhenVoiceChatMuted) {
-                UnityTranslateClient.shouldTranscribe = !it.isDisabled
-            }
-        }
-
-        registration.registerEvent(VoicechatServerStartedEvent::class.java) {
-            voiceChatServer = it.voicechat
-        }
+    fun isPlayerDeafened(player: ServerPlayer): Boolean {
+        return if (usesSimpleVoiceChat)
+            SimpleVoiceChatCompat.isPlayerDeafened(player)
+        else if (usesPlasmoVoice)
+            PlasmoVoiceChatCompat.isPlayerDeafened(player)
+        else
+            false
     }
 
-    companion object {
-        lateinit var voiceChatServer: VoicechatServerApi
-
-        fun getNearbyPlayers(source: ServerPlayer): List<ServerPlayer> {
-            if (isPlayerDeafened(source))
-                return listOf(source)
-
-            return source.serverLevel().getPlayers {
-                (!isPlayerDeafened(it) &&
-                        ((it.distanceToSqr(source) <= voiceChatServer.voiceChatDistance * voiceChatServer.voiceChatDistance && areBothSpectator(it, source)) ||
-                                playerSharesGroup(it, source))
-                )
-                || it == source
-            }
-        }
-
-        fun playerSharesGroup(player: ServerPlayer, other: ServerPlayer): Boolean {
-            val firstGroup = voiceChatServer.getConnectionOf(player.uuid)?.group ?: return false
-            val secondGroup = voiceChatServer.getConnectionOf(other.uuid)?.group ?: return false
-
-            return firstGroup.id == secondGroup.id
-        }
-
-        fun isPlayerDeafened(player: ServerPlayer): Boolean {
-            return voiceChatServer.getConnectionOf(player.uuid)?.isDisabled == true
-        }
-
-        fun areBothSpectator(player: ServerPlayer, other: ServerPlayer): Boolean {
-            if (player.gameMode.gameModeForPlayer == GameType.SPECTATOR && other.gameMode.gameModeForPlayer == GameType.SPECTATOR)
-                return true
-            else if (player.gameMode.gameModeForPlayer == GameType.SPECTATOR && other.gameMode.gameModeForPlayer != GameType.SPECTATOR)
-                return false
-
+    fun areBothSpectator(player: ServerPlayer, other: ServerPlayer): Boolean {
+        if (player.gameMode.gameModeForPlayer == GameType.SPECTATOR && other.gameMode.gameModeForPlayer == GameType.SPECTATOR)
             return true
-        }
+        else if (player.gameMode.gameModeForPlayer == GameType.SPECTATOR && other.gameMode.gameModeForPlayer != GameType.SPECTATOR)
+            return false
+
+        return true
     }
 }
