8c98497de42b71fd1f5974a9cb2670543abd65a0
Naz <bluspringindohq@gmail.com>
2024-09-04 15:03:10 +0800

    Improve HTTP POST requests in translations

---
+++ common/src/main/kotlin/xyz/bluspring/unitytranslate/util/HttpHelper.kt
@@ -0,0 +1,54 @@
+package xyz.bluspring.unitytranslate.util
+
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import xyz.bluspring.unitytranslate.UnityTranslate
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+object HttpHelper {
+    fun post(uri: String, body: JsonObject, headers: Map<String, String> = mapOf()): JsonElement {
+        val url = URL(uri)
+        var connection: HttpURLConnection? = null
+
+        val data = body.toString().toByteArray(Charsets.UTF_8)
+
+        try {
+            connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+
+            connection.setRequestProperty("Accept", "application/json")
+            connection.setRequestProperty("Content-Type", "application/json")
+            headers.forEach { (key, value) ->
+                connection.setRequestProperty(key, value)
+            }
+
+            connection.doOutput = true
+            connection.setFixedLengthStreamingMode(data.size)
+
+            val outputStream = BufferedOutputStream(connection.outputStream)
+
+            outputStream.write(data)
+            outputStream.flush()
+            outputStream.close()
+
+            if (connection.responseCode / 100 != 2) {
+                throw Exception("Failed to load $uri (code: ${connection.responseCode})")
+            } else {
+                val inputStream = BufferedInputStream(connection.inputStream)
+                val reader = inputStream.bufferedReader(Charsets.UTF_8)
+                val result = JsonParser.parseReader(reader)
+
+                reader.close()
+                inputStream.close()
+
+                return result
+            }
+        } finally {
+            connection?.disconnect()
+        }
+    }
+}
